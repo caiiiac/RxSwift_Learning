dc275daeccfb1fd2043e91977f900ee450fec593
caijq <caiiiac@163.com>
2018-08-07 15:44:07 +0800

    13-map

--- 13-intermediate-rxcocoa/Wundercast/Wundercast/Controllers/Extensions/MKMapView+Rx.swift
+++ 13-intermediate-rxcocoa/Wundercast/Wundercast/Controllers/Extensions/MKMapView+Rx.swift
@@ -24,3 +24,50 @@ import Foundation
 import MapKit
 import RxSwift
 import RxCocoa
+
+
+extension MKMapView: HasDelegate {
+    public typealias Delegate = MKMapViewDelegate
+}
+
+class RxMKMapViewDelegateProxy: DelegateProxy<MKMapView, MKMapViewDelegate>, DelegateProxyType, MKMapViewDelegate {
+    public weak private(set) var mapView: MKMapView?
+    
+    public init(mapView: ParentObject) {
+        self.mapView = mapView
+        super.init(parentObject: mapView, delegateProxy: RxMKMapViewDelegateProxy.self)
+    }
+    
+    static func registerKnownImplementations() {
+        self.register { (mapView) -> RxMKMapViewDelegateProxy in
+            RxMKMapViewDelegateProxy(mapView: mapView)
+        }
+    }
+}
+
+extension Reactive where Base: MKMapView {
+    public var delegate: DelegateProxy<MKMapView, MKMapViewDelegate> {
+        return RxMKMapViewDelegateProxy.proxy(for: base)
+    }
+    
+    public func setDelegate(_ delegate: MKMapViewDelegate) -> Disposable {
+        return RxMKMapViewDelegateProxy.installForwardDelegate(delegate, retainDelegate: false, onProxyForObject: self.base)
+    }
+    
+    var overlays: Binder<[MKOverlay]> {
+        return Binder(self.base) { mapView, overlays in
+            mapView.removeOverlays(mapView.overlays)
+            mapView.addOverlays(overlays)
+        }
+    }
+    
+    public var regionDidChangeAnimated: ControlEvent<Bool> {
+        let source = delegate
+            .methodInvoked(#selector(MKMapViewDelegate.mapView(_:regionDidChangeAnimated:)))
+            .map { (parameters) -> Bool in
+                return (parameters[1] as? Bool) ?? false
+            }
+        return ControlEvent(events: source)
+    }
+    
+}
