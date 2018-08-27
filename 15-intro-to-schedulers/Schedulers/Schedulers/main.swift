f20a6e4f4ef80227b24048cb6c964a2d925d2797
caijq <caiiiac@163.com>
2018-08-27 17:13:23 +0800

    15-observeOn

--- 15-intro-to-schedulers/Schedulers/Schedulers/main.swift
+++ 15-intro-to-schedulers/Schedulers/Schedulers/main.swift
@@ -34,3 +34,24 @@ animal
   .dump()
   .dumpingSubscription()
   .disposed(by: bag)
+
+
+let fruit = Observable<String>.create { observer in
+    observer.onNext("[apple]")
+    sleep(2)
+    observer.onNext("[pineapple]")
+    sleep(2)
+    observer.onNext("strawberry")
+    return Disposables.create()
+}
+
+fruit
+    .subscribeOn(globalScheduler)
+    .dump()
+    .observeOn(MainScheduler.instance)
+    .dumpingSubscription()
+    .disposed(by: bag)
+
+RunLoop.main.run(until: Date(timeIntervalSinceNow: 13))
+
+
